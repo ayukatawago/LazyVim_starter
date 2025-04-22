60b9c954aa650f4328b9e56d7b8817aee3581dd9
takuya-ogawa <takuya.ogawa@lycorp.co.jp>
2025-04-22 09:09:41 +0900

    Add option for bufferline.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,11 +1,21 @@
 return {
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = { event = "notify", find = "No information available" },
-      opts = { skip = true },
-    })
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = { event = "notify", find = "No information available" },
+        opts = { skip = true },
+      })
 
-    opts.presets.lsp_doc_border = true
-  end,
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
 }
